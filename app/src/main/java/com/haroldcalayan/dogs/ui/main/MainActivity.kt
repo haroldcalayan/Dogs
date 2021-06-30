f774db6ef004843f731065af029eb36b614d8cfb
Harold Calayan <haroldcalayan@gmail.com>
2021-06-30 12:28:12 +0800

    :hammer: Move main activity inside ui/main package

--- app/src/main/java/com/haroldcalayan/dogs/ui/main/MainActivity.kt
+++ app/src/main/java/com/haroldcalayan/dogs/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.haroldcalayan.dogs
+package com.haroldcalayan.dogs.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haroldcalayan.dogs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
